24457210c42ba079ac5314f6eb46c888972a7a9c
natalie-moffa-wvu <nfm00003@mix.wvu.edu>
2026-04-22 02:04:58 +0000

    init file

---
+++ src/wvu_ieng_331_m2_21/src/wvu_ieng_331_m2_21/__init__.py
@@ -0,0 +1,11 @@
+Milestone 2: Python Pipeline Package
+
+This package contains the ETL + analytics pipeline for Olist seller analysis.
+
+Main entry point:
+    pipeline.main()
+
+
+from .pipeline import main
+
+__all__ = ["main"]
